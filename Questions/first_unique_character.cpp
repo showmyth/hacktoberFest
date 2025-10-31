17c7ac2fadaef279cfa8ba5dde0c15bc0fd47024
Ricardo Saldaña <54114934+Rick1551@users.noreply.github.com>
2025-10-31 13:18:33 +0000

    First unique

--- Questions/first_unique_character.cpp
+++ Questions/first_unique_character.cpp
@@ -1,2 +1,20 @@
-// Given a string, return the index of the first non-repeating character. If it
-// doesn’t exist, return -1.
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+int firstUniqueChar(const std::string& s) {
+    std::unordered_map<char, int> count;
+    
+    //Contar ocurrencias
+    for(char c : s) {
+        count[c]++;
+    }
+
+    //Buscar el primer caracter unico
+    for(size_t i = 0; i < s.size(); ++i) {
+        if(count[s[i]] == 1) {
+            return static_cast<int>(i);
+        }
+    }
+    return -1;
+}
