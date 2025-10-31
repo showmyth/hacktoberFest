5fb9349ee29adcf616fc02e128883b7575256f6f
Ricardo Saldaña <54114934+Rick1551@users.noreply.github.com>
2025-10-31 13:29:12 +0000

    Sort_stack

--- Questions/sort_stack.cpp
+++ Questions/sort_stack.cpp
@@ -1,2 +1,24 @@
-// Given a stack of integers, sort it in ascending order using only stack
-// operations(no other data structures allowed)
+#include <iostream>
+#include <stack>
+
+//Inserta un elemento en la posicion correcta dentro de una pila ordenada
+void insertSorted(std::stack<int>& s, int val) {
+    if (s.empty() || val > s.top()) {
+        s.push(val);
+        return;
+    }
+
+    int temp = s.top();
+    s.pop();
+    insertSorted(s, val);
+    s.push(temp);
+}
+    //Ordena la pila usando recursion
+    void sortStack(std::stack<int>& s) {
+        if(!s.empty()){
+            int temp = s.top();
+            s.pop();
+            sortStack(s);
+            insertSorted(s,temp);
+        }
+    }
